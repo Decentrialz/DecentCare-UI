4c065a337039cd8f27b5dd964c7666b72bd0464a
manunidhi999 <manunidhichinthireddy@gmail.com>
2026-09-22 16:27:50 +0530

    Update footer and other content changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
               <span className="text-xs font-semibold text-[#FFFFFF]">Decent<span className="text-accent">Care</span></span>
             </Link>
             <p className="text-sm text-[#FBFCFD]/60 ">
-             DecentCare is the behavioral intelligence layer for Indian healthcare. We help clinics hospitals, and doctors see every patient, understand who's ready, and act - so they grow smarter.
+             DecentCare is the behavioral intelligence layer for Indian healthcare. We help clinics, hospitals, and doctors see every patient, understand who's ready, and act - so they grow smarter.
             </p>
             {/* Social Links */}
             <div className="flex gap-4 mt-[10%]">
